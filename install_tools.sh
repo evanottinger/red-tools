b64de5b5ba56dbbc33124badd3138e848553d3cf
evan.ottinger <jottinger@alkugov.com>
2021-08-11 15:17:48 -0400

    Add script to install masscan

--- install_tools.sh
+++ install_tools.sh
@@ -5,8 +5,11 @@
 #
 # Author: Evan Ottinger -- @OttySec
 
-# Update Database
-apt update
+# Update Installed packages
+apt update && upgrade -y
+
+# Install Dependencies
+apt install git gcc make libpcap-dev
 
 # Subdomain Enumeration Tools
 ## Install Sublist3r
@@ -23,3 +26,12 @@ apt install snapd
 service snapd start
 snap install amass
 echo ["[+] amass installed!"]
+
+# Port Scanners and Network Enumeration Tools
+## Install Masscan
+echo "[>] Install Masscan. . ."
+git clone https://github.com/robertdavidgraham/masscan.git /opt/masscan
+cd /opt/masscan
+make -j
+cd ~
+echo ["[+] Masscan installed!"]
