34fe3b57e0308f80f3b287beaa2db16eeb802a22
evan.ottinger <jottinger@alkugov.com>
2021-08-11 15:09:17 -0400

    Add script to install amass

--- install_tools.sh
+++ install_tools.sh
@@ -5,10 +5,21 @@
 #
 # Author: Evan Ottinger -- @OttySec
 
-# Install Sublist3r
+# Update Database
+apt update
+
+# Subdomain Enumeration Tools
+## Install Sublist3r
 echo "[>] Instaling Sublist3r. . ."
 git clone https://github.com/aboul3la/Sublist3r.git /opt/Sublist3r
 sudo pip -r install /opt/Sublist3r/requirements.txt
 ln -s /opt/Sublist3r/sublist3r.py $(echo $PATH | cut -d ":" -f 1)
 echo "[+] Sublist3r installed!"
 
+## Install amass
+### dependent upon snap package manager
+echo "[>] Installing amass. . ."
+apt install snapd
+service snapd start
+snap install amass
+echo ["[+] amass installed!"]
